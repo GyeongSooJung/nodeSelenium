45c1d500fc3d41cf51ee22fb625080f3fa3b0c4d
GyeongSooJung <gsjung06@naver.com>
2022-08-10 17:05:19 +0900

    alertHandler

--- tests/modules/selenium/alertHandler.test.ts
+++ tests/modules/selenium/alertHandler.test.ts
@@ -47,29 +47,73 @@ import {
     popupClose
 
  } from '../../../modules';
-  
+
+ // window type 선언
+declare const window: typeof globalThis;
 
  describe('Module alertHandler', () => {
     // 웹드라이버 설정
     let driver : WebDriver;
 
-    beforeAll(async () => {
+    beforeEach(async () => {
         driver = await getDriverHandler();
+        driver.get('https://testpages.herokuapp.com/styled/alerts/alert-test.html')
     })
 
-    afterAll(async () => {
-        await driver.quit()
+    afterEach(async () => {
+        await driver.close()
     })
 
-    test('alert 확인버튼 테스트', async () => {
+    test('alert 테스트 ( alert 텍스트가 잘 나오는지 )', async () => {
+        try {
+            await (await findElementById(driver,'alertexamples')).click();
 
-    })
+            // alert 창 뜰 때까지 기다림
+            await driver.wait(until.alertIsPresent());
 
-    test('alert 닫기버튼 테스트', async () => {
+            // alert 로 드라이버 이동
+            let alert = await driver.switchTo().alert();
 
+            // alertText
+            let alertText = await alert.getText();
+
+            // 확인버튼 클릭
+            await alert.accept();
+
+            // 다시 원래 컨텐츠로 드라이버 이동
+            await driver.switchTo().defaultContent();
+
+            expect(alertText).toEqual('I am an alert box!');
+        }
+        catch(Err) {
+            loggerDebug.info(Err)
+        }
     })
 
     test('prompt 테스트', async () => {
+        try {
+            await (await findElementById(driver,'promptexample')).click();
 
+            // alert 창 뜰 때까지 기다림
+            await driver.wait(until.alertIsPresent());
+
+            // alert 로 드라이버 이동
+            let alert = await driver.switchTo().alert();
+
+            // alert text 사용하는 곳
+            let alertText = await alert.getText();
+
+            // 확인버튼 클릭
+            await alert.accept();
+
+            // 다시 원래 컨텐츠로 드라이버 이동
+            await driver.switchTo().defaultContent();
+
+            expect(alertText).toEqual('I prompt you');
+
+        }
+        catch(Err) {
+            loggerDebug.info(Err)
+        }
     })
 })
